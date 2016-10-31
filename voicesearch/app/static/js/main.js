16d3ba0a85366b27cb80f09182a91f91a3d2b478
Benjamin Hermberg <ben@hermberg.ch>
2016-10-31 19:13:47 +0100

    ...

--- voicesearch/app/static/js/main.js
+++ voicesearch/app/static/js/main.js
@@ -97,23 +97,21 @@ function updateStatus(status) {
 
 }
 
-function executeSearch(status) {
-    var statusS = document.getElementById("search_input");
-    statusS.value = status;
+function executeSearch(value) {
+    var fieldInput = document.getElementById("search_input");
+    fieldInput.value = value;
 
-    console.log('executing search.... for keyword: ' + status);
-
-    getProducts(status, function(products) {
+    getProducts(value, function(products) {
         currentVue.updateAll(products);
     });
-
-    //document.forms['press_button'].submit();
 }
 
 function startRecording(event) {
     event.preventDefault();
     var el = event.currentTarget;
+    var searchField = document.getElementById('form_text_input');
     el.classList.add("recording");
+    searchField.classList.add("recording");
     audioRecorder.clear();
     audioRecorder.record();
 }
@@ -121,23 +119,12 @@ function startRecording(event) {
 function stopRecording(ev) {
     event.preventDefault();
     var el = event.currentTarget;
+    var searchField = document.getElementById('form_text_input');
     el.classList.remove("recording");
+    searchField.classList.remove("recording");
     audioRecorder.stop();
     audioRecorder.getBuffers(gotBuffers);
 }
-
-/* TODO:
-
- - offer mono option
- - "Monitor input" switch
- */
-
-function saveAudio() {
-    audioRecorder.exportWAV(doneEncoding);
-    // could get mono instead by saying
-    // audioRecorder.exportMonoWAV( doneEncoding );
-}
-
 
 function gotBuffers(buffers) {
     var canvas = document.getElementById("wavedisplay");
@@ -148,90 +135,6 @@ function gotBuffers(buffers) {
     // so here's where we should set up the download.
     audioRecorder.exportMonoWAV(sendBlob);
     //apiaiSend();
-}
-
-
-function toggleRecording(event) {
-    var el = event.target;
-    if (el.classList.contains("recording")) {
-
-        console.log('end recording');
-        // stop recording
-        audioRecorder.stop();
-        el.classList.remove("recording");
-        el.src = "/static/mic.png";
-        document.getElementById('circle').style.visibility = "hidden";
-        audioRecorder.getBuffers(gotBuffers);
-    } else {
-
-    }
-}
-
-function convertToMono(input) {
-    var splitter = audioContext.createChannelSplitter(2);
-    var merger = audioContext.createChannelMerger(2);
-
-    input.connect(splitter);
-    splitter.connect(merger, 0, 0);
-    splitter.connect(merger, 0, 1);
-    return merger;
-}
-
-function cancelAnalyserUpdates() {
-    window.cancelAnimationFrame(rafID);
-    rafID = null;
-}
-
-function updateAnalysers(time) {
-    if (!analyserContext) {
-        var canvas = document.getElementById("analyser");
-        // canvasWidth = canvas.width;
-        // canvasHeight = canvas.height;
-        analyserContext = canvas.getContext('2d');
-    }
-
-    // analyzer draw code here
-    {
-        var SPACING = 3;
-        var BAR_WIDTH = 1;
-        var numBars = Math.round(canvasWidth / SPACING);
-        var freqByteData = new Uint8Array(analyserNode.frequencyBinCount);
-
-        analyserNode.getByteFrequencyData(freqByteData);
-
-        analyserContext.clearRect(0, 0, canvasWidth, canvasHeight);
-        analyserContext.fillStyle = '#F6D565';
-        analyserContext.lineCap = 'round';
-        var multiplier = analyserNode.frequencyBinCount / numBars;
-
-        // Draw rectangle for each frequency bin.
-        for (var i = 0; i < numBars; ++i) {
-            var magnitude = 0;
-            var offset = Math.floor(i * multiplier);
-            // gotta sum/average the block, or we miss narrow-bandwidth spikes
-            for (var j = 0; j < multiplier; j++)
-                magnitude += freqByteData[offset + j];
-            magnitude = magnitude / multiplier;
-            var magnitude2 = freqByteData[i * multiplier];
-            analyserContext.fillStyle = "hsl( " + Math.round((i * 360) / numBars) + ", 100%, 50%)";
-            analyserContext.fillRect(i * SPACING, canvasHeight, BAR_WIDTH, -magnitude);
-        }
-    }
-
-    rafID = window.requestAnimationFrame(updateAnalysers);
-}
-
-function toggleMono() {
-    if (audioInput != realAudioInput) {
-        audioInput.disconnect();
-        realAudioInput.disconnect();
-        audioInput = realAudioInput;
-    } else {
-        realAudioInput.disconnect();
-        audioInput = convertToMono(realAudioInput);
-    }
-
-    audioInput.connect(inputPoint);
 }
 
 function gotStream(stream) {
